6f7e291abaa9f0ab9ec3f6e54c132804599254fa
moensn <niels.moens@delaware.pro>
2023-03-08 14:30:48 +0100

    end of dependency injection with Annotations

--- spring-demo-annotations/src/main/java/com/luv2code/springdemoannotations/RandomFortuneService.java
+++ spring-demo-annotations/src/main/java/com/luv2code/springdemoannotations/RandomFortuneService.java
@@ -2,12 +2,25 @@ package com.luv2code.springdemoannotations;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomFortuneService implements FortuneService {
-    
+
+    //  Create an array of strings
+    private final String[] data = {
+            "beware of the wolf", "fons makker", "fons de spons"
+    };
+
+    //  Create a random number gen
+    private final Random myRandom = new Random();
 
     @Override
     public String getFortune() {
-        return null;
+        //  pick a random string from the array
+        final int index = myRandom.nextInt(data.length);
+
+        final String theFortune = data[index];
+        return theFortune;
     }
 }
--- spring-demo-annotations/src/main/java/com/luv2code/springdemoannotations/TennisCoach.java
+++ spring-demo-annotations/src/main/java/com/luv2code/springdemoannotations/TennisCoach.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
-    @Qualifier("happyFortuneService")
+    @Qualifier("randomFortuneService")
     @Autowired
     private FortuneService fortuneService;
 
