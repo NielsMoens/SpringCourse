284ac62a4b750aadb7a7335a3fe764f6674684e3
moensn <niels.moens@delaware.pro>
2023-03-08 15:36:26 +0100

    end of dependency injection with Annotations

--- spring-demo-annotations/src/main/java/com/luv2code/springdemoannotations/AnnotationBeanScopeDemoApp.java
+++ spring-demo-annotations/src/main/java/com/luv2code/springdemoannotations/AnnotationBeanScopeDemoApp.java
@@ -1,7 +1,26 @@
 package com.luv2code.springdemoannotations;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationBeanScopeDemoApp {
-    public static void main(String[] args) {
-        
+    public static void main(final String[] args) {
+
+        //  load spring config file
+        final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        final Coach theCoach = context.getBean("tennisCoach", Coach.class);
+        final Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+
+        //  check to see if there the same
+        final boolean result = (theCoach == alphaCoach);
+
+        //  print out the results
+        System.out.println("\nPointing to the same object: " + result);
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+        System.out.println("\nMemory location for alphaCoach: " + alphaCoach + "\n");
+
+        // close the context
+        context.close();
     }
 }
