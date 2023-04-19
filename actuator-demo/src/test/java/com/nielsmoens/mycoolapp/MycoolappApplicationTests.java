710d08ecdbf176092023a1086cb52eb5cb11b9f4
moensn <niels.moens@delaware.pro>
2023-04-19 18:46:17 +0200

    return a specific student

---
+++ actuator-demo/src/test/java/com/nielsmoens/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nielsmoens.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dev-tools-demo/src/main/java/com/nielsmoens/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,26 @@
+package com.nielsmoens.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that returns "hello world"
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello world!";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "run barry run";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "JUST DO it";
+    }
+}
