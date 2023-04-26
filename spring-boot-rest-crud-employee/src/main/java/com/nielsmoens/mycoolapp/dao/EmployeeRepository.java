99c504328d588d2efa06f72677cc87923e86b418
moensn <niels.moens@delaware.pro>
2023-04-26 11:22:04 +0200

    Configs and Sorting

--- spring-boot-rest-crud-employee/src/main/java/com/nielsmoens/mycoolapp/dao/EmployeeRepository.java
+++ spring-boot-rest-crud-employee/src/main/java/com/nielsmoens/mycoolapp/dao/EmployeeRepository.java
@@ -4,8 +4,7 @@ package com.nielsmoens.mycoolapp.dao;
 import com.nielsmoens.mycoolapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-    // that's it ... no need to write any code LOL!
 
 }
